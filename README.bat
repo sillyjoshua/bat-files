bcdf067f3457e1eac2775f96649e71eb1364b30e
sillyjoshua <joshuaisverycool2024@gmail.com>
2025-02-19 09:36:54 +0000

    this is an example to get you started!

--- README.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-echo hi this is just some text coded in BAT
-
-echo and this is where you will find my batch files!
---
+++ example.bat
@@ -0,0 +1,10 @@
+@echo off
+
+title EXAMPLE #1
+
+echo edit this as much as you want this is an example 
+pause
+
+echo copy and paste or download!
+
+exit
